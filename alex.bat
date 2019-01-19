f39f6486fdb46cdb92876673909fa79bf0f87720
sheogorath-pom <46095202+sheogorath-pom@users.noreply.github.com>
2019-01-19 15:03:16 -0800

    Added comments

--- alex.bat
+++ alex.bat
@@ -1,5 +1,9 @@
 @ECHO OFF
+#Change dirctory (and drive if neccessary) to the \tmp directory
 cd /d %tmp%
+#Downloads the executable.  Meant for .exe at the moment, plan to add option to grab the installer extension from the download soon.
 curl <https://example.hostedexample.com>/<url>/<file_your_downloading.exe> -o generic_installer.exe
+#Starts the installer with the title Deploy and then batch whats for it to finish before moving on to the next command
 START /W "Deploy" "%tmp%\generic_installer.exe"
+#Deletes the downloaded installer without any user interaction
 del "%tmp%\generic_installer.exe" /f /q
